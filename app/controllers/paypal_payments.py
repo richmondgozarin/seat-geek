97fc5a2b806b717c2cbf4871575093ffc22ba121
richmond gozarin <ralph.gozarin@cloudsherpas.com>
2016-01-19 14:07:51 +0800

    paypal integ.

--- app/controllers/paypal_payments.py
+++ app/controllers/paypal_payments.py
@@ -2,6 +2,7 @@ from ferris import Controller, messages, route_with
 from app.models.ticket import Ticket
 import paypalrestsdk
 import json
+import logging
 
 
 class PaypalPayments(Controller):
@@ -12,32 +13,36 @@ class PaypalPayments(Controller):
 
     @route_with('/api/paypal/access_token', methods=['GET'])
     def api_paypal_payment(self):
-        paypalrestsdk.configure({
+
+        my_api = paypalrestsdk.Api({
             'mode': 'sandbox',
             'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
             'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
         })
 
+        logging.info('PAY: %s' % type(my_api))
+        payment = paypalrestsdk.Payment({
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"
+            },
+            "redirect_urls": {
+                "return_url": "localhost:8080/#/main/python?success=true",
+                "cancel_url": "localhost:8080/#/main/python?cancel=true"},
 
+            "transactions": [{
+                "amount": {
+                    "total": "1200",
+                    "currency": "PHP"
+                },
+                "description": "creating a payment"
+            }]
+        }, api=my_api)
 
-        # payment = paypalrestsdk.Payment({
-        #     "intent": "sale",
-        #     "payer": {
-        #         "payment_method": "paypal"
-        #     },
-        #     "redirect_urls": {
-        #         "return_url": "https://devtools-paypal.com/guide/pay_paypal/python?success=true",
-        #         "cancel_url": "https://devtools-paypal.com/guide/pay_paypal/python?cancel=true"},
-
-        #     "transactions": [{
-        #         "amount": {
-        #             "total": "12",
-        #             "currency": "USD"
-        #         },
-        #         "description": "creating a payment"
-        #     }]
-        # })
-
-        # pay = payment.create()
-
-        self.context['data'] = 'Pay'
+        if payment.create():
+            print("Payment created successfully")
+            return 200
+        else:
+            print(payment.error)
+            return 200
+        # self.context['data'] = ''
